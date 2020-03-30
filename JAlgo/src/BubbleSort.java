cb6087cfba53c988cae820ba50e2f4e2d82c9709
胡钰维 <huyuwei1996@gmail.com>
2020-03-30 12:36:58 +0800

    first commit

--- JAlgo/src/BubbleSort.java
+++ JAlgo/src/BubbleSort.java
@@ -1,12 +1,21 @@
+import sun.jvm.hotspot.debugger.posix.elf.ELFSectionHeader;
+
 import java.util.Arrays;
 
 public class BubbleSort {
-    public void BubbleSort(int arr[]) {
+    // 时间复杂度O(n^2)
+    public static int[] BubbleSort(int arr[]) {
         int temp = 0;
-        // if i:0～lenth-1，j:0～length-1-i
-        // if i:1～lenth，  j:0～length-i
+//        i =[0, len - 1)
+//        j =[0, len - 1 - i)
+
+//        i =[1, len)
+//        j =[0, len - i)
+        // 外层循环控制比较轮数
         for (int i = 0; i < arr.length - 1; i++) {
+            // 内层循环控制每轮比较次数
             for (int j = 0; j < arr.length - 1 - i; j++) {
+                // 如果前面数比后面数大，则交换
                 if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -17,13 +26,37 @@ public class BubbleSort {
             System.out.println(Arrays.toString(arr));
             System.out.println("第" + (i + 1) + "趟👆");
         }
-
-
-        public static void main (String[]args){
-//        int arr[] = {3, 9, -1, 10, -2};
-            int arr[] = {1, 3, 5, 7, 7, 3, 9, 5, 3};
-
-            BubbleSort(arr);
-        }
+        return arr;
     }
+
+    public static void main(String[] args) {
+//        int arr[] = {3, 9, -1, 10, -2};
+        int arr[] = {1, 3, 5, 7, 7, 3, 9, 5, 3};
+
+//        BubbleSort(arr);
+        BubbleSortUp(arr);//优化后的冒泡排序
+    }
+
+    public static int[] BubbleSortUp(int arr[]) {
+        int temp = 0;
+        for (int i = 1; i < arr.length; i++) {
+            boolean flag = false;
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = true;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            if (!flag) {
+                break;
+            }
+//            System.out.println(Arrays.toString(arr));
+//            System.out.println("第" + (i) + "趟👆");
+        }
+        return arr;
+    }
+
 }
+
